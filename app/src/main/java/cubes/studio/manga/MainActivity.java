be76409cb93d087003ae0fd4fde3f812428ef6d1
XZman <xueyuqijim@foxmail.com>
2017-02-09 23:27:18 +0800

    all four fragments in main activity

--- app/src/main/java/cubes/studio/manga/MainActivity.java
+++ app/src/main/java/cubes/studio/manga/MainActivity.java
@@ -1,5 +1,6 @@
 package cubes.studio.manga;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
@@ -7,11 +8,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.Button;
+import layout.BookmarkPage;
+import layout.CategoryPage;
+import layout.MainPage;
+import layout.MyInfoPage;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FrameLayout currentView;
+    private MainPage mMainPage;
+    private CategoryPage mCategoryPage;
+    private BookmarkPage mBookmarkPage;
+    private MyInfoPage mMyInfoPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +28,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setDefaultView();
+        setBottomBarOnClickListener();
+    }
 
+    private void setDefaultView() {
+        if (mMainPage == null)
+            mMainPage = MainPage.newInstance();
+        setFragmentAsCurrentView(mMainPage);
+    }
+
+    private void setBottomBarOnClickListener() {
+        View.OnClickListener ocl = v -> {
+            int id = v.getId();
+            switch (id) {
+                case R.id.main_page_button:
+                    if (mMainPage == null)
+                        mMainPage = MainPage.newInstance();
+                    setFragmentAsCurrentView(mMainPage);
+                    break;
+                case R.id.category_page_button:
+                    if (mCategoryPage == null)
+                        mCategoryPage = CategoryPage.newInstance();
+                    setFragmentAsCurrentView(mCategoryPage);
+                    break;
+                case R.id.bookmark_page_button:
+                    if (mBookmarkPage == null)
+                        mBookmarkPage = BookmarkPage.newInstance();
+                    setFragmentAsCurrentView(mBookmarkPage);
+                    break;
+                case R.id.myInfo_page_button:
+                    if (mMyInfoPage == null)
+                        mMyInfoPage = MyInfoPage.newInstance();
+                    setFragmentAsCurrentView(mMyInfoPage);
+                    break;
+            }
+        };
+        findViewById(R.id.main_page_button).setOnClickListener(ocl);
+        findViewById(R.id.category_page_button).setOnClickListener(ocl);
+        findViewById(R.id.bookmark_page_button).setOnClickListener(ocl);
+        findViewById(R.id.myInfo_page_button).setOnClickListener(ocl);
+    }
+
+    private void setFragmentAsCurrentView(Fragment fragment) {
         FragmentManager fm = getFragmentManager();
         FragmentTransaction transc = fm.beginTransaction();
-        transc.replace(R.id.current_view, IndexPage.newInstance());
+        transc.replace(R.id.current_view, fragment);
         transc.commit();
     }
 
