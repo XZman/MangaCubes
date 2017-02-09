0e3e77b7767a19ab545d34a0a9bacd70fb341c12
XZman <xueyuqijim@foxmail.com>
2017-02-09 15:07:37 +0800

    fragment&bottombar

--- app/src/main/java/cubes/studio/manga/MainActivity.java
+++ app/src/main/java/cubes/studio/manga/MainActivity.java
@@ -3,16 +3,15 @@ package cubes.studio.manga;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class IndexActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_index);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
