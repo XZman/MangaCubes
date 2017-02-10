9713103f935efbbfacfec18e6c11afee46a1d86c
XZman <xueyuqijim@foxmail.com>
2017-02-10 09:36:28 +0800

    delete unused import

--- app/src/main/java/cubes/studio/manga/MainActivity.java
+++ app/src/main/java/cubes/studio/manga/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import layout.BookmarkPage;
 import layout.CategoryPage;
 import layout.MainPage;
